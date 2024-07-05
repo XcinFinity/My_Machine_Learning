cb35af439f5c89c624e7330a435c581cadf2ea7e
XcinFinity <lyndon.bin.914@gmail.com>
2024-07-05 20:56:34 +0800

    李沐-动手学深度学习-5-深度学习计算

---
+++ Limu/5_Deep Learning_PyTorch.py
@@ -0,0 +1,220 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+net = nn.Sequential(nn.Linear(20, 256), nn.ReLU(), nn.Linear(256, 10))
+
+X = torch.rand(2, 20)
+net(X)
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.hidden = nn.Linear(20, 256)
+        self.out = nn.Linear(256, 10)
+
+    def forward(self, X):
+        return self.out(F.relu(self.hidden(X)))
+
+net = MLP()
+net(X)
+
+class MySequential(nn.Module):
+    def __init__(self, *args):
+        super(MySequential, self).__init__()
+        for block in args:
+            self._modules[block] = block
+
+    def forward(self, X):
+        for block in self._modules.values():
+            X = block(X)
+        return X
+
+net = MySequential(nn.Linear(20, 256), nn.ReLU(), nn.Linear(256, 10))
+net(X)
+
+class FixedHiddenMLP(nn.Module):
+    def __init__(self):
+        super(FixedHiddenMLP, self).__init__()
+        self.rand_weight = torch.rand((20, 20), requires_grad=False)
+        self.linear = nn.Linear(20, 20)
+
+    def forward(self, X):
+        X = self.linear(X)
+        X = F.relu(torch.mm(X, self.rand_weight) + 1)
+        X = self.linear(X)
+        while X.abs().sum() > 1:
+            X /= 2
+        return X.sum()
+
+net = FixedHiddenMLP()
+net(X)
+
+class NestMLP(nn.Module):
+    def __init__(self):
+        super(NestMLP, self).__init__()
+        self.net = nn.Sequential(nn.Linear(20, 64), nn.ReLU(), nn.Linear(64, 32), nn.ReLU())
+        self.linear = nn.Linear(32, 16)
+
+    def forward(self, X):
+        return self.linear(self.net(X))
+
+chimera = nn.Sequential(NestMLP(), nn.Linear(16, 20), FixedHiddenMLP())
+chimera(X)
+
+
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
+X = torch.rand(size=(2, 4))
+net(X)
+
+print(net[2].state_dict())
+
+print(type(net[2].bias))
+print(net[2].bias) 
+print(net[2].bias.data)
+
+print(net[2].weight.grad == None) 
+
+print(*[(name, param.shape) for name, param in net[0].named_parameters()])
+print(*[(name, param.shape) for name, param in net.named_parameters()])
+
+net.state_dict()['2.bias'].data
+
+def block1():
+    return nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4), nn.ReLU())
+
+def block2():
+    net = nn.Sequential()
+    for i in range(4):
+        net.add_module(f'block {i}', block1())
+    return net
+
+rgnet = nn.Sequential(block2(), nn.Linear(4, 1))
+rgnet(X)
+
+print(rgnet)
+
+def init_normal(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0, std=0.01)
+        nn.init.zeros_(m.bias)
+
+net.apply(init_normal)
+net[0].weight.data[0], net[0].bias.data[0]
+
+def init_constant(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 1)
+        nn.init.zeros_(m.bias)
+
+net.apply(init_constant)
+net[0].weight.data[0], net[0].bias.data[0]
+
+def xavier(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+
+def init_42(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 42)
+
+net[0].apply(xavier)
+net[2].apply(init_42)
+print(net[0].weight.data[0])
+print(net[2].weight.data)
+
+def my_init(m):
+    if type(m) == nn.Linear:
+        print(
+            "Init",
+            *[(name, param.shape) for name, param in m.named_parameters()][0]
+        )
+        nn.init.uniform_(m.weight, -10, 10)
+        m.weight.data *= m.weight.data.abs() >= 5
+
+net.apply(my_init)
+net[0].weight[:2]
+
+net[0].weight.data[:] += 1
+net[0].weight.data[0, 0] = 42
+net[0].weight.data[0]
+
+shared = nn.Linear(8, 8)
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), shared, nn.ReLU(), shared, nn.ReLU(), nn.Linear(8, 1))
+net(X)
+print(net[2].weight.data[0] == net[4].weight.data[0])
+net[2].weight.data[0, 0] = 100
+print(net[2].weight.data[0] == net[4].weight.data[0])
+
+
+class CenteredLayer(nn.Module):
+    def __init__(self):
+        super(CenteredLayer, self).__init__()
+
+    def forward(self, X):
+        return X - X.mean()
+    
+layer = CenteredLayer()
+layer(torch.FloatTensor([1, 2, 3, 4, 5]))
+
+net = nn.Sequential(nn.Linear(8, 128), CenteredLayer())
+
+Y = net(torch.rand(4, 8))
+Y.mean()
+
+class Mylinear(nn.Module):
+    def __init__(self, in_units, units):
+        super(Mylinear, self).__init__()
+        self.weight = nn.Parameter(torch.randn(in_units, units))
+        self.bias = nn.Parameter(torch.randn(units,))
+
+    def forward(self, X):
+        linear = torch.matmul(X, self.weight.data) + self.bias.data
+        return F.relu(linear)
+    
+dense = Mylinear(5, 3)
+dense.weight
+
+dense(torch.rand(2, 5))
+
+net = nn.Sequential(Mylinear(64, 8), Mylinear(8, 1))
+net(torch.rand(2, 64))
+
+
+x = torch.arange(4)
+torch.save(x, 'x-file')
+
+X2 = torch.load('x-file')
+X2
+
+y = torch.zeros(4)
+torch.save([x, y], 'x-files')
+x2, y2 = torch.load('x-files')
+(x2, y2)
+
+mydict = {'x': x, 'y': y}
+torch.save(mydict, 'mydict')
+mydict2 = torch.load('mydict')
+mydict2
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.hidden = nn.Linear(20, 256)
+        self.out = nn.Linear(256, 10)
+
+    def forward(self, X):
+        return self.out(F.relu(self.hidden(X)))
+    
+net = MLP()
+X = torch.randn(size=(2, 20))
+Y = net(X)
+
+torch.save(net.state_dict(), 'mlp.params')
+
+clone = MLP()
+clone.load_state_dict(torch.load('mlp.params'))
+clone.eval()
+
+Y_clone = clone(X)
+Y_clone == Y
